a4f9264d0c8eb7531c56bcfca0db4b9da3524fe7
Will D <123596349+pleplu@users.noreply.github.com>
2024-01-29 20:37:40 -0500

    Add files via upload

---
+++ Exercise-1.5/1.5-Practice Task 2/1.5-Practice-Task-2.py
@@ -0,0 +1,39 @@
+# Define __sub__() for heights in feet and inches.
+
+# Subtract 3 feet 9 inches from 5 feet 10 inches.
+
+class Height(object):
+    def __init__(self, feet, inches):
+        self.feet = feet
+        self.inches = inches
+
+    def __str__(self):
+        output = str(self.feet) + " feet, " + str(self.inches) + " inches"
+        return output
+    
+    def __sub__(self, other):
+        # Converting both objects' heights into inches
+        height_A_inches = self.feet * 12 + self.inches
+        height_B_inches = other.feet * 12 + other.inches
+
+        # Adding them up
+        total_height_inches = height_A_inches - height_B_inches
+
+        # Getting the output in feet
+        output_feet = total_height_inches // 12
+
+        # Getting the output in inches
+        output_inches = total_height_inches - (output_feet * 12)
+
+        # Returning the final output as a new Height object
+        return Height(output_feet, output_inches)
+    
+person_A_height = Height(3, 9)
+person_B_height = Height(5, 10)
+height_sum = person_B_height - person_A_height
+
+print("Total height:", height_sum)
+
+# Output: 
+
+# Total height: 2 feet, 1 inches
